bc5b4a80c316b0d788405e2873f9672c08beedb5
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2024-09-08 15:25:05 +0530

    reduced space complexity using voting

--- stringAndArrays/7_majority-element.js
+++ stringAndArrays/7_majority-element.js
@@ -4,6 +4,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+/*
 let majorityElement = function (nums) {
   const ht = {};
 
@@ -18,6 +19,24 @@ let majorityElement = function (nums) {
     }
   }
 };
+*/
+
+let majorityElement = function (nums) {
+  let majority = nums[0];
+  let votes = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (votes === 0) {
+      votes++
+      majority = nums[i];
+    } else if (majority !== nums[i]) {
+      votes--;
+    } else {
+      votes++
+    }
+  }
+  return majority;
+}
 
 console.log(majorityElement([3, 2, 3])); // 3
+console.log(majorityElement([3, 2, 3, 1])); // 3
 console.log(majorityElement([3, 2, 3, 1, 1, 1])); // 1
